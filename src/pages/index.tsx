214cfe8a0434e011cda037565ae50670f5853b09
Ben Trinh <bentrinh@pit-m3d.com>
2021-04-15 23:45:53 +1000

    Add PWA. Convert NavBar to TS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 
@@ -8,20 +9,8 @@ export default function Home() {
 
   return (
   <>
-  <Navbar transparent />
-    <main className="profile-page">
-          <section className="relative block" style={{ height: 500 }}>
-            <div
-              className="absolute top-0 w-full h-full bg-center bg-cover"
-              style={{
-                backgroundImage:
-                  // "url('https://wonderfulengineering.com/wp-content/uploads/2014/04/code-wallpaper-18.png')"
-                  // eslint-disable-next-line no-template-curly-in-string
-                  "url(/img/code-wallpaper-18.png)",
-              }}
-            />{" "}
-          </section>{""}
-    </main>
+  <Header title="Index"/>
+    
   <Footer />
   </>
   )}
