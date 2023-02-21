07a378c353de58880487bbe723a5dffdcef57e9f
Ben Trinh <huantrinh1802@gmail.com>
2023-02-21 23:32:45 +1100

    [dev] Added layout using Skeleton

---
+++ src/contents/blogs/first-blog.md
@@ -0,0 +1,9 @@
+---
+title: First blog
+description: ""
+date: 2023-02-21T11:36:54.546Z
+preview: ""
+draft: ""
+tags: ""
+categories: ""
+---
---
+++ src/lib/layout/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar>
+  <svelte:fragment slot="lead">(icon)</svelte:fragment>
+  <svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+  <!-- <svelte:fragment slot="headline" /> -->
+</AppBar>
