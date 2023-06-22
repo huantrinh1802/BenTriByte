0edd62bee631b98b430ca6854ba640bb1f57d870
Ben Trinh <huantrinh1802@gmail.com>
2023-06-22 11:17:14 +1000

    [dev] Just dev

---
+++ apps/website/src/lib/components/History.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let content: any = null;
+</script>
+
+<div class="flex flex-col print:block gap-4 print:gap-1 print:break-before-auto">
+  <h3 class="print:!text-lg">{content.title}</h3>
+  {#if content.subtitle}
+    <h4 class="print:!text-base">{content.subtitle}</h4>
+  {/if}
+  <div class="flex justify-between">
+    <div class="text-sm">{content.timeline}</div>
+    <div class="text-sm">{content.location}</div>
+  </div>
+  {#if content.summary}
+    <div>{content.summary}</div>
+  {/if}
+  {#if content.tasks}
+    <ul class="!list-disc list">
+      {#each content.tasks as task}
+        <li class="!whitespace-normal">
+          <span>○</span>
+          <span class="print:text-xs">{task}</span>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</div>
