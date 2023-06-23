a47969d1b5952863d5685be61a8d3861cac10acb
Ben Trinh <huantrinh1802@gmail.com>
2023-06-23 10:43:04 +1000

    [dev] Added printable on mobile device

--- apps/website/src/lib/components/History.svelte
+++ apps/website/src/lib/components/History.svelte
@@ -2,17 +2,17 @@
   export let content: any = null;
 </script>
 
-<div class="flex flex-col print:block gap-4 print:gap-1 print:break-before-auto">
+<div class="flex flex-col print:block gap-3 print:gap-1 print:break-before-auto">
   <h3 class="print:!text-lg">{content.title}</h3>
   {#if content.subtitle}
-    <h4 class="print:!text-base">{content.subtitle}</h4>
+    <h4 class="print:!text-base text-gray-200 print:text-gray-800">{content.subtitle}</h4>
   {/if}
-  <div class="flex justify-between">
+  <div class="flex justify-between text-gray-200 print:text-gray-800">
     <div class="text-sm">{content.timeline}</div>
     <div class="text-sm">{content.location}</div>
   </div>
   {#if content.summary}
-    <div>{content.summary}</div>
+    <div class="py-2">{content.summary}</div>
   {/if}
   {#if content.tasks}
     <ul class="!list-disc list">
