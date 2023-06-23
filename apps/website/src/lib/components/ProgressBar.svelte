f8bc12b7bcccd553ba1789ab61b694a355753421
Ben Trinh <huantrinh1802@gmail.com>
2023-06-23 10:43:04 +1000

    [dev] Added printable on mobile device

--- apps/website/src/lib/components/ProgressBar.svelte
+++ apps/website/src/lib/components/ProgressBar.svelte
@@ -1,6 +1,73 @@
 <script lang="ts">
   export let title: string = '';
   export let percentage: number = 50;
+  function hslToHex(h, s, l) {
+    // Convert hue to degrees
+    h %= 360;
+    if (h < 0) {
+      h += 360;
+    }
+
+    // Normalize saturation and lightness values
+    s = Math.max(0, Math.min(s, 100)) / 100;
+    l = Math.max(0, Math.min(l, 100)) / 100;
+
+    // Calculate RGB values
+    const c = (1 - Math.abs(2 * l - 1)) * s;
+    const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+    const m = l - c / 2;
+
+    let r, g, b;
+    if (h >= 0 && h < 60) {
+      r = c;
+      g = x;
+      b = 0;
+    } else if (h >= 60 && h < 120) {
+      r = x;
+      g = c;
+      b = 0;
+    } else if (h >= 120 && h < 180) {
+      r = 0;
+      g = c;
+      b = x;
+    } else if (h >= 180 && h < 240) {
+      r = 0;
+      g = x;
+      b = c;
+    } else if (h >= 240 && h < 300) {
+      r = x;
+      g = 0;
+      b = c;
+    } else {
+      r = c;
+      g = 0;
+      b = x;
+    }
+
+    // Convert RGB values to HEX
+    const componentToHex = (component) => {
+      const hex = Math.round(component * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+
+    const hexR = componentToHex(r + m);
+    const hexG = componentToHex(g + m);
+    const hexB = componentToHex(b + m);
+
+    return '#' + hexR + hexG + hexB;
+  }
+  function getColor(percentage, toHex = true) {
+    if (percentage >= 100) {
+      percentage = 100;
+    } else if (percentage <= 0) {
+      percentage = 1;
+    }
+    let hue = (190 / 100) * percentage;
+    if (toHex) {
+      return hslToHex(hue, 100, 30);
+    }
+    return `hsl(${hue}, 100%, 25%)`;
+  }
 </script>
 
 <div class="flex justify-between mb-1">
@@ -8,5 +75,7 @@
   <!-- <span class="text-sm font-medium text-blue-700 dark:text-white">{percentage}%</span> -->
 </div>
 <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-  <div class={`bg-blue-600 h-2.5 rounded-full w-[${percentage}%]`} />
+  <div
+    class={`bg-blue-600 h-2.5 rounded-full`}
+    style={`width: ${percentage}%; background-color: ${getColor(percentage, false)}`} />
 </div>
