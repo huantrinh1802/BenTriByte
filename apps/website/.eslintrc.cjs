f8c64c882c4b6aefc329bd1c9be147b400f5dc10
Ben Trinh <huantrinh1802@gmail.com>
2023-09-20 17:40:31 +1000

    [dev] upgrade all deps

---
+++ apps/website/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:svelte/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
+  // settings: {
+  //   'svelte3/typescript': () => require('typescript'),
+  // },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+};
--- apps/website/src/hooks.server.ts
+++ apps/website/src/hooks.server.ts
@@ -1,5 +1,4 @@
 import { redirect, type HandleServerError } from '@sveltejs/kit';
-import { get } from 'svelte/store';
 
 export const handleError = (() => {
   // example integration with https://sentry.io/
