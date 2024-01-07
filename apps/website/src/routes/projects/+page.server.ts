6435c401323dade37d664d0e991c8e448ace2f55
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-01-07 16:22:50 +1100

    [dev] Added graphql for github projects but not visible
    - Added footer

---
+++ apps/website/src/routes/projects/+page.server.ts
@@ -0,0 +1,41 @@
+import { GITHUB_TOKEN } from '$env/static/private';
+import type { PageServerLoad } from './$types';
+import { request, gql } from 'graphql-request';
+type Language = {
+  name: string;
+  color: string;
+};
+type Repository = {
+  description: string;
+  name: string;
+  homepageUrl: string;
+  primaryLanguage: Language;
+  languages: { nodes: Language[] };
+};
+export const load = (async () => {
+  const document = gql`
+    query Repositories {
+      viewer {
+        repositories(first: 100, privacy: PUBLIC) {
+          nodes {
+            description
+            name
+            homepageUrl
+            primaryLanguage {
+              color
+              name
+            }
+            languages(first: 100) {
+              nodes {
+                color
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+  const repositories: { viewer: { repositories: { nodes: Repository[] } } } = await request('https://api.github.com/graphql', document, null, { Authorization: `Bearer ${GITHUB_TOKEN}` });
+  return { repositories: repositories.viewer.repositories.nodes };
+}) satisfies PageServerLoad;
