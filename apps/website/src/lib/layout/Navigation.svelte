b42fa0646f7982184587143db8eaf5fb69d402de
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-01-07 16:22:50 +1100

    [dev] Added graphql for github projects but not visible
    - Added footer

--- apps/website/src/lib/layout/Navigation.svelte
+++ apps/website/src/lib/layout/Navigation.svelte
@@ -4,36 +4,36 @@
   const drawerStore = getDrawerStore();
 </script>
 
-<ul class="flex flex-col gap-4 px-4">
+<div class="flex flex-col gap-4 px-4">
   {#each menuItems as item}
     {#if item.href != undefined}
-      <li>
-        <a
-          on:click={() => drawerStore.close()}
-          href={item.href}>{item.name}</a
-        >
-      </li>
+      <a
+        class="rounded-sm p-2 hover:bg-primary-600 hover:text-white"
+        on:click={() => drawerStore.close()}
+        href={item.href}>{item.name}</a
+      >
     {:else}
       <Accordion>
-        <AccordionItem padding="p-0">
+        <AccordionItem
+          padding="p-2"
+          hover="hover:bg-primary-600 hover:text-white"
+        >
           <svelte:fragment slot="summary">{item.name}</svelte:fragment>
           <svelte:fragment slot="content">
-            <ul class="flex flex-col divide-y divide-black rounded-md bg-surface-200 px-4 py-4 dark:divide-white dark:bg-surface-700">
+            <div class="grid divide-y divide-black rounded-md bg-surface-200 p-4 dark:divide-white dark:bg-surface-700">
               {#each item.subItems as subItem}
-                <li class="flex py-2">
-                  <a
-                    on:click={() => drawerStore.close()}
-                    class="w-full"
-                    href={subItem.href}
-                  >
-                    {subItem.name}
-                  </a>
-                </li>
+                <a
+                  on:click={() => drawerStore.close()}
+                  class="w-full p-2 hover:bg-primary-600 hover:text-white"
+                  href={subItem.href}
+                >
+                  {subItem.name}
+                </a>
               {/each}
-            </ul>
+            </div>
           </svelte:fragment>
         </AccordionItem>
       </Accordion>
     {/if}
   {/each}
-</ul>
+</div>
