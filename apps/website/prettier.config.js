9d65900d123c1114d35237249f0bd19773f80000
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-01-11 23:17:46 +1100

    [chore] Update prettier

---
+++ apps/website/prettier.config.js
@@ -0,0 +1,26 @@
+const config = {
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 200,
+  pluginSearchDirs: false,
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleAttributePerLine: true,
+  useTabs: false,
+  plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+  overrides: [
+    { files: "*.svelte", options: { parser: "svelte" } },
+  { files: ["*.svx"], options: { parser: "mdx" } }
+  ]
+}
+
+export default config;
