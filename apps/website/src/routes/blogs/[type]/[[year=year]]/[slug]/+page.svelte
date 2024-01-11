9fc01807478dca66b5d0f11126776bd60682d2b7
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-01-12 09:33:00 +1100

    [dev] Added toc

--- apps/website/src/routes/blogs/[type]/[[year=year]]/[slug]/+page.svelte
+++ apps/website/src/routes/blogs/[type]/[[year=year]]/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { PageData } from './$types';
-
+  import { TableOfContents, popup, tocCrawler } from '@skeletonlabs/skeleton';
   // import type { PageData } from './$types';
 
   export let data: PageData;
@@ -8,18 +8,39 @@
 </script>
 
 <svelte:head><title>{metadata.title}</title></svelte:head>
-<div class="prose p-10 dark:prose-invert">
+<div class="relative prose p-10 dark:prose-invert">
+  <button
+    type="button"
+    use:popup={{ event: 'click', target: 'toc', placement: 'bottom' }}
+    class="variant-filled btn-icon fixed top-20 left-4">TOC</button
+  >
+  <div data-popup="toc" class="bg-surface-700 px-4 py-4 w-max max-w-[80%] rounded-sm max-h-[60dvh] overflow-auto">
+    <TableOfContents/>
+
+  </div>
   <h1 class="text-center">{metadata.title}</h1>
-  <div class="mx-auto flex w-[75ch] justify-between">
-    <div class="flex gap-4">
+  <div class="flex flex-col justify-between gap-4 sm:mx-auto sm:w-[75ch]">
+    <div class="flex flex-wrap gap-4">
       {#each metadata.tags as tag}
         <a href={`/blogs?tag=${tag}`}>#{tag}</a>
       {/each}
     </div>
-    <div>{new Date(metadata.date).toLocaleDateString()}</div>
+    <div class="ml-auto">{new Date(metadata.date).toLocaleDateString()}</div>
   </div>
-  <div class="mx-auto w-[75ch]">
-    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-    {@html content}
+  <div class="grid py-4 prose-headings:my-0 md:grid-cols-[20ch_1fr]">
+    <div class="relative hidden md:block">
+      <TableOfContents
+        class="sticky top-0"
+        regionList="pl-0"
+        regionListItem="list-disc"
+      />
+    </div>
+    <div
+      class="mx-auto w-[min(100%,80ch)]"
+      use:tocCrawler={{ mode: 'generate' }}
+    >
+      <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+      {@html content}
+    </div>
   </div>
 </div>
