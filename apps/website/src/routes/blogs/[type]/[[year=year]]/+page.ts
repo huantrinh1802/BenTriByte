d63c2f44bb7199e1ef10cf92ba384d6e05025982
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-01-13 17:43:47 +1100

    [fix] Fixed base is not defined during build time.

--- apps/website/src/routes/blogs/[type]/[[year=year]]/+page.ts
+++ apps/website/src/routes/blogs/[type]/[[year=year]]/+page.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerData } from './$types';
+import type { PageData } from './$types';
+import { browser } from '$app/environment';
 import { base } from '$app/paths';
+import { PUBLIC_SITE_URL } from '$env/static/public';
 
 export const load = (async ({ parent, params }) => {
   const blogGroups = await parent();
@@ -10,6 +12,7 @@ export const load = (async ({ parent, params }) => {
       return splittedDate[0] == params.year;
     });
     return { blogs, type: params.type, year: params?.year, years: blogGroups['blogs'][params['type']].years };
+  } else {
+    throw redirect(302, `${params.type}/${blogGroups['blogs'][params['type']].years.at(-1)}`);
   }
-  throw redirect(302, `${base}/blogs/${params.type}/${blogGroups['blogs'][params['type']].years.at(-1)}`);
-}) satisfies PageServerData;
+}) satisfies PageData;
