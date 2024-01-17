35fb30d4023fe3d9d79bb13ddd9a217b82b70ddb
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-01-17 22:02:43 +1100

    [dev] Improved blog feed

--- apps/website/src/routes/blogs/feed/+server.ts
+++ apps/website/src/routes/blogs/feed/+server.ts
@@ -2,13 +2,19 @@ import { PUBLIC_SITE_URL } from '$env/static/public';
 import type { BlogMetadata } from '$lib/types/blog';
 export const prerender = true;
 function generate_url(loc: string, lastmod: string, metadata: BlogMetadata) {
-  return `<item>
-  <guid>${loc}</guid>
-  <title>${metadata.title}</title>
-  <link>${loc}</link>
-  <pubDate>${lastmod}</pubDate>
-  <description>
-  <![CDATA[<img height="100" width="100" src="${PUBLIC_SITE_URL}/${metadata.preview}"/><p>${metadata.description}</p><span>${metadata.summary}</p>]]></description>
+  return `
+  <item>
+    <guid>${loc}</guid>
+    <title>${metadata.title}</title>
+    <link>${loc}</link>
+    <pubDate>${lastmod}</pubDate>
+    <description>
+      <![CDATA[
+        <img height="100" width="100" src="${PUBLIC_SITE_URL}/${metadata.preview}"/>
+        <h3>${metadata.description}</h3>
+        <p>${metadata.summary}</p>
+      ]]>
+    </description>
   </item>`;
 }
 export const GET = async () => {
@@ -30,12 +36,12 @@ export const GET = async () => {
     `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
         <channel>
           <title>BenTri Byte Blogs</title>
-          <link>${PUBLIC_SITE_URL}</link>
+          <link>${PUBLIC_SITE_URL}/blogs</link>
           <description>A blog built with SvelteKit about tech and stuff!</description>
           <image>
             <url>${PUBLIC_SITE_URL}/favicon.png</url>
             <title>BenTri Byte Icon</title>
-            <link>${PUBLIC_SITE_URL}</link>
+            <link>${PUBLIC_SITE_URL}/blogs</link>
           </image>
         </channel>
         ${urls}
