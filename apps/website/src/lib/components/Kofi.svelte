e3d4eab1ddb2dd54b3bbdb01365401826e4a52e1
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-03-25 15:06:24 +1100

    [modify] Update Ko-fi link and layout in Kofi.svelte, add new coffee link - Change the layout and styling of the Ko-fi link - Add a new link to BuyMeACoffee.com with BMAF image asset - Add new image asset BMAF.webp

--- apps/website/src/lib/components/Kofi.svelte
+++ apps/website/src/lib/components/Kofi.svelte
@@ -1,4 +1,11 @@
-<a class="flex items-center" href="https://ko-fi.com/W7W4VVT9J">
-  <img height="48" width="48" src="/images/kofi.webp" alt="Buy me a coffee!" />
-  <span>Buy me a coffee!</span>
-</a>
+<div class=" items-center gap-2">
+  <span> Buy me a coffee! </span>
+  <div class="flex items-center gap-4">
+    <a class="flex h-10 w-10 items-center rounded-full" href="https://ko-fi.com/W7W4VVT9J">
+      <img src="/images/kofi.webp" alt="Buy me a Ko-fi!" />
+    </a>
+    <a class="flex h-10 w-10 items-center rounded-full dark:bg-white" href="https://www.buymeacoffee.com/bentribyte">
+      <img src="/images/BMAF.webp" alt="Buy me a coffee!" />
+    </a>
+  </div>
+</div>
