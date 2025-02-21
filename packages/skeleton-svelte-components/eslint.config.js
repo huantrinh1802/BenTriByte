190110ffe49b61a74aacae8081e470003b3971d0
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2025-02-21 19:09:14 +1100

    [dev] Migrate ESLint configuration to use modern setup
    - Deleted `.eslintrc.json` and `.eslintignore` from multiple locations
    - Added `eslint.config.js` to use new configuration format
    - Updated dependencies related to ESLint in `package.json` files
    - Upgraded various `eslint`, `eslint-config-prettier`, and `eslint-plugin-svelte` dependencies
    - Removed `postcss.config.cjs` and added `postcss.config.js` with modern setup
    - Updated other package dependencies for compatibility with newer ESLint version
    - Updated `pnpm-lock.yaml` with new package versions and configurations

---
+++ packages/skeleton-svelte-components/eslint.config.js
@@ -0,0 +1,33 @@
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import * as svelteParser from 'svelte-eslint-parser';
+import * as typescriptParser from '@typescript-eslint/parser';
+export default [
+  {
+    ignores: [
+      '.DS_Store',
+      'node_modules',
+      'build',
+      '.svelte-kit',
+      'package',
+      '.env',
+      '.env.*',
+      '!env.example', // Negation isn't directly supported, see note below
+      'dist',
+      'pnpm-lock.yaml',
+      'package-lock.json',
+      'yarn.lock',
+    ],
+  },
+  ...eslintPluginSvelte.configs['flat/prettier'],
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: typescriptParser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.svelte'],
+      },
+    },
+  },
+];
