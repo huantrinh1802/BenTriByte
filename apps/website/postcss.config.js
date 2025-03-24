072e0489e01755c8ca8a60e9aa11f90d43d49486
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2025-03-24 14:24:45 +1100

    [modified] Update ESLint config and dependencies
    - Added `.ts` to ESLint's `extraFileExtensions`
    - Added `typescript-svelte-plugin` to `package.json` dependencies
    - Modified `postcss.config.js` to adjust plugin configuration
    - Updated `pnpm-lock.yaml` with new versions including `turbo` and `typescript-svelte-plugin`

--- apps/website/postcss.config.js
+++ apps/website/postcss.config.js
@@ -1,13 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-const config = {
-  plugins: [
-    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    tailwindcss(),
-    //But others, like autoprefixer, need to run after,
-    autoprefixer,
-  ],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
