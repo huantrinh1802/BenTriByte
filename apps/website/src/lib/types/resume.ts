3ea44966a108ba291e51aabfa722a5192a985c13
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2025-03-24 20:12:20 +1100

    [modify] layout server and page components
    - Implement GraphQL request to fetch resume data from Github in `layout.server.ts`
    - Add support for fetching resumes for Ben Trinh and Amber Duong from the file_storage repository
    - Define resume type and modify data binding in `page.svelte`
    - Safeguard against missing skillSet title in `page.svelte`

--- apps/website/src/lib/types/resume.ts
+++ apps/website/src/lib/types/resume.ts
@@ -7,10 +7,90 @@ export type Content = {
 };
 
 export type Resume = {
-  profile: object;
-  contacts: object;
-  skills: { [key: string]: { title: string; professional: Content[]; casual: Content[] } };
-  experiences: object;
-  educations: object;
-  languages: object;
+  profile: {
+    name: string;
+    fullName: string;
+    job: string;
+    image: string;
+    themeColour: string;
+    showPhoto: boolean;
+  };
+  summary: string;
+  contacts: Array<{
+    href: string;
+    type: string;
+    text: string;
+    printText?: string;
+  }>;
+  experiences: Array<{
+    title: string;
+    subtitle: string;
+    timeline: string;
+    location: string;
+    summary: string;
+    tasks: Array<string>;
+  }>;
+  educations: Array<{
+    title: string;
+    timeline: string;
+    location: string;
+  }>;
+  skills: {
+    languages: {
+      title: string;
+      professional: Array<{
+        percentage: number;
+        title: string;
+        contents: Array<string>;
+      }>;
+      casual: Array<{
+        percentage: number;
+        title: string;
+        contents: Array<string>;
+      }>;
+    };
+    techs: {
+      title: string;
+      professional: Array<{
+        percentage: number;
+        title: string;
+        contents: Array<string>;
+      }>;
+      casual: Array<{
+        percentage: number;
+        title: string;
+        contents: Array<string>;
+      }>;
+    };
+    clouds: {
+      title: string;
+      professional: Array<{
+        percentage: number;
+        title: string;
+        contents: Array<string>;
+      }>;
+      casual: Array<{
+        percentage: number;
+        title: string;
+        contents: Array<string>;
+      }>;
+    };
+  };
+  languages: Array<{
+    title: string;
+    percentage: number;
+  }>;
+  references: Array<{
+    name: string;
+    title: string;
+    company: string;
+    mobile: {
+      href: string;
+      text: string;
+    };
+    email: {
+      href: string;
+      text: string;
+    };
+  }>;
 };
