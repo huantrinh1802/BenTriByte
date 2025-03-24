fcbcffd77b1716e4ed9b33534a654f60d6fc6a91
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2025-03-24 20:12:19 +1100

    [dev] Instead of popup, use string to show when copied link

--- apps/website/src/lib/components/RssFeed.svelte
+++ apps/website/src/lib/components/RssFeed.svelte
@@ -1,24 +1,9 @@
 <script lang="ts">
   import RSS from '~icons/foundation/rss';
   import { PUBLIC_SITE_URL } from '$env/static/public';
-  import { popup, type PopupSettings } from '@skeletonlabs/skeleton';
   export let style = '';
   let trigger: HTMLButtonElement;
-  const popupClick: PopupSettings = {
-    event: 'click',
-    target: 'popupClick',
-    placement: 'bottom',
-    state: async (event) => {
-      if (event.state) {
-        await new Promise((resolve) => {
-          setTimeout(() => {
-            trigger.click();
-            resolve;
-          }, 500);
-        });
-      }
-    },
-  };
+  let copied = false;
 </script>
 
 <button
@@ -26,14 +11,20 @@
   title="Subscribe to RSS"
   aria-label="Subscribe to RSS"
   on:click={async () => {
+    copied = !copied;
     navigator.clipboard.writeText(`${PUBLIC_SITE_URL}/blogs/feed`);
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        copied = !copied;
+        resolve;
+      }, 800);
+    });
   }}
-  class={`btn btn-icon h-10 w-10 bg-primary-500 ${style}`}
-  use:popup={popupClick}
+  class={`btn btn-icon h-10 w-fit bg-primary-500 ${style}`}
 >
-  <RSS class="text-2xl text-white" />
+  {#if copied}
+    <p class="px-2 py-1 text-base">Copied link</p>
+  {:else}
+    <RSS class="px-2 text-2xl text-white" />
+  {/if}
 </button>
-<div class="card variant-filled-primary px-4 py-1" data-popup="popupClick">
-  <p class="p-0 text-sm">Copied link</p>
-  <!-- <div class="arrow variant-filled-primary" /> -->
-</div>
