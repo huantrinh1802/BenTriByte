0c8550328b7f6f34078d34f5539441b1b8b2a75a
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2025-03-24 20:12:17 +1100

    [dev] Added graphql for github projects but not visible
    - Added footer

---
+++ apps/website/src/routes/projects/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  export let data;
+
+  // pull the store reference from the route props
+</script>
+
+<div class="grid rounded-md bg-secondary-400 p-2">
+  {#each data.repositories as repo}
+    <div class="card prose dark:prose-invert">
+      <h2 class="text-center">{repo.name}</h2>
+      <div>{repo.description}</div>
+      <div>
+        <a
+          target="_blank"
+          href={repo.homepageUrl}
+          class="btn">Visit the page</a
+        >
+      </div>
+      {#each repo.languages.nodes as language}
+        <div
+          class="language-wrapper"
+          style={`--backgroundColor: ${language.color}`}
+        >
+          <p class="language">{language.name}</p>
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style lang="postcss">
+  .language-wrapper {
+    mix-blend-mode: screen;
+    background-color: var(--backgroundColor);
+  }
+  .language {
+    color: black;
+    mix-blend-mode: difference;
+  }
+</style>
